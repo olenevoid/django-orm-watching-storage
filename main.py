e485fe6549d7043ca034891240903637ce6298c5
olenevoid <mazekunov@gmail.com>
2025-07-04 11:38:08 +0500

    Add printing of all fields that are not a relation

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
+    first = Passcard.objects.first()
+    print('owner_name', first)
+    for field in first._meta.get_fields():
+        if not field.is_relation:
+            print(field.verbose_name, getattr(first, field.name))
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
