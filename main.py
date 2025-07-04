b8e4c1c9f587c546cdec5ffc671f080ab52df861
olenevoid <mazekunov@gmail.com>
2025-07-04 23:14:46 +0500

    Use count to get the number of active passcards

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь    
-    active_passcards = Passcard.objects.filter(is_active=True).all()
+    active_passcards_number = Passcard.objects.filter(is_active=True).count()
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    print('Количество активных пропусков:', len(active_passcards))
+    print('Количество активных пропусков:', active_passcards_number)
